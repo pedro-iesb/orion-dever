593aa033fcb36b5f4dc08558d217437443d1a660
Mateus Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-03-30 21:59:57 -0300

    first commit

---
+++ componentes/Pagina.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Cabecalho from './Cabecalho'
+import Rodape from './Rodape'
+
+const Pagina = () => {
+
+ return (
+     <>
+        <Cabecalho/>
+        <div className='bg-secondary text-white py-3 text-center mb-3'>
+            <h1>Página Inicial</h1>
+        </div>
+        <Rodape/>
+    </>
+    )
+}
+
+export default Pagina
--- pages/pagina2.js
+++ pages/pagina2.js
@@ -1,20 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Alert, Button, Container } from 'react-bootstrap';
 import Cabecalho from "../componentes/Cabecalho";
-import Pagina from '../componentes/Pagina';
 import Rodape from "../componentes/Rodape";
 
 export default function Home() {
   return (
       <>
-      
-      <Pagina/>
+      <Cabecalho/>
+
+      <div className="bg-secondary text-white py-3 text-center mb-3">
+         <h1>Página 2</h1>
+      </div>
 
       <Container>
-        <h1>Hello world!</h1>
-        <p>Qualquer texto</p>
-        <p>Qualquer texto</p>
-        <p>Qualquer texto</p>
+        <h1>Página 2</h1>
+        <Rodape/>
       </Container>
       </>
   )
